a90a8e908dc5f9f7391464806f4946ff8a7d3c4c
Daniel Wilczak <141693938+sfc-gh-dwilczak@users.noreply.github.com>
2023-09-19 23:25:40 +0000

    Updated toml array.

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(
     description=
-        "A package for the simple local management of snowpark for python connections."
+        "A package for the simple local management of snowpark for python connections.",
 )
